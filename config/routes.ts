6038368a90834bba847fad651e19e57d549c795f
chromer <chuliang2013@live.cn>
2022-06-17 18:31:06 +0800

    feat: 🎸 init commit

---
+++ config/routes.ts
@@ -0,0 +1,53 @@
+﻿export default [
+  {
+    path: '/user',
+    layout: false,
+    routes: [
+      {
+        name: 'login',
+        path: '/user/login',
+        component: './user/Login',
+      },
+      {
+        component: './404',
+      },
+    ],
+  },
+  {
+    path: '/welcome',
+    name: '首页',
+    icon: 'home',
+    component: './Welcome',
+  },
+  {
+    path: '/admin',
+    name: '管理员功能',
+    icon: 'crown',
+    access: 'canAdmin',
+    component: './Admin',
+    routes: [
+      {
+        path: '/admin/sub-page',
+        name: '管理权限',
+        icon: 'smile',
+        component: './Welcome',
+      },
+      {
+        component: './404',
+      },
+    ],
+  },
+  {
+    name: '表单',
+    icon: 'table',
+    path: '/list',
+    component: './TableList',
+  },
+  {
+    path: '/',
+    redirect: '/welcome',
+  },
+  {
+    component: './404',
+  },
+];
